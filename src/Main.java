b02cf8a78a743a89d27561222fb2dee8d7fdb2b0
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-12-06 16:19:41 +0100

    Update Main.java
    added filter category and price

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -13,15 +14,19 @@ public class Main
     {
         Prodotto iphone = new Prodotto("iphone", "smartphone", 1348.99);
         Prodotto harrypotter = new Prodotto("harry potter", "libro", 15.99);
-        Prodotto lordofrings=new Prodotto("lord of rings","libro",25.99);
+        Prodotto lordofrings=new Prodotto("lord of rings","libro",250.99);
         Prodotto narhinel = new Prodotto("narhinel", "baby", 14.99);
+        Prodotto narnia=new Prodotto("narnia","libro",340.90);
 
         ArrayList <Prodotto>prodotti=new ArrayList<>();
         prodotti.add(iphone);
         prodotti.add(harrypotter);
         prodotti.add(narhinel);
         prodotti.add(lordofrings);
-        prodotti.stream().filter(categoria->categoria.getCategoria().equals("libro")).forEach(System.out::println);
+        prodotti.add(narnia);
+        Predicate <Prodotto> categoria=categoria1->categoria1.getCategoria().equals("libro");
+        Predicate <Prodotto>prezzo=prezzo1->prezzo1.getPrezzo()>100;
 
+        prodotti.stream().filter(categoria.and(prezzo)).forEach(System.out::println);
     }
 }
